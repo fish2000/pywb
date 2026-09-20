dd295a16d1511acf9ba43a90bb17b410ad38a15c
Alexander Böhn <fish2000@gmail.com>
2026-09-20 19:33:31 -0400

    Renamed function `load_requirements(…)` to `load_text_as_list(…)` … it has a generalized use case, q.v. commit notes supra.

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ def generate_git_hash_py(pkg, filename='git_hash.py'):
         pass
 
 
-def load_requirements(filename):
+def load_text_as_list(filename):
     with open(filename, 'rt') as fh:
         requirements = fh.read().rstrip().split('\n')
     return requirements
@@ -119,7 +119,7 @@ setup(
         ('sample_archive/text_content',
          glob.glob('sample_archive/text_content/*')),
     ],
-    install_requires=load_requirements('requirements.txt'),
+    install_requires=load_text_as_list('requirements.txt'),
     extras_require={
         "i18n":  [
             "babel",
@@ -128,7 +128,7 @@ setup(
         ],
     },
     python_requires='>=3.9,<3.15',
-    tests_require=load_requirements("test_requirements.txt"),
+    tests_require=load_text_as_list("test_requirements.txt"),
     cmdclass={'test': PyTest},
     test_suite='',
     entry_points="""
